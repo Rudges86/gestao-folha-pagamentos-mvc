a382ba4702ca785cc5a0518a3d5eae47b8e42328
Rudge Santos <rudges86@gmail.com>
2026-04-17 16:09:48 -0300

    Início do projeto

---
+++ gestao-folha-pagamentos/src/main/java/br/com/service/gestao_folha_pagamentos/GestaoFolhaPagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.service.gestao_folha_pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFolhaPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFolhaPagamentosApplication.class, args);
+	}
+
+}
---
+++ gestao-folha-pagamentos/src/test/java/br/com/service/gestao_folha_pagamentos/GestaoFolhaPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.service.gestao_folha_pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFolhaPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
